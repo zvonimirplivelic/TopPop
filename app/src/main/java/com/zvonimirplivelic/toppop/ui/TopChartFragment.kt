a664d8b41b4642fc7527a58e62d6ea55c1cd338a
Zvonimir <zvonimirplivelic91@gmail.com>
2022-06-24 12:57:14 +0200

    Displaying data in recycler view

--- app/src/main/java/com/zvonimirplivelic/toppop/ui/TopChartFragment.kt
+++ app/src/main/java/com/zvonimirplivelic/toppop/ui/TopChartFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.zvonimirplivelic.toppop.R
 import com.zvonimirplivelic.toppop.util.Resource
 import com.zvonimirplivelic.toppop.util.TopPopViewModel
@@ -17,6 +20,11 @@ class TopChartFragment : Fragment() {
 
     private lateinit var viewModel: TopPopViewModel
 
+    private lateinit var topChartAdapter: TopChartAdapter
+    private lateinit var recyclerView: RecyclerView
+
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,6 +33,14 @@ class TopChartFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_top_chart, container, false)
 
         viewModel = ViewModelProvider(this)[TopPopViewModel::class.java]
+        recyclerView = view.findViewById(R.id.rv_top_chart)
+        progressBar = view.findViewById(R.id.progress_bar)
+
+        topChartAdapter = TopChartAdapter()
+        recyclerView.apply {
+            adapter = topChartAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
 
         viewModel.getTopChart()
 
@@ -32,13 +48,17 @@ class TopChartFragment : Fragment() {
 
             when (response) {
                 is Resource.Success -> {
+                    progressBar.isVisible = false
+                    recyclerView.isVisible = true
+
                     response.data?.let { topChart ->
-                        Timber.d("TopChart: $topChart")
+                        topChartAdapter.differ.submitList(topChart.tracks.data)
                     }
                 }
 
                 is Resource.Error -> {
-
+                    progressBar.isVisible = false
+                    recyclerView.isVisible = false
                     response.message?.let { message ->
                         Toast.makeText(activity, "An error occured: $message", Toast.LENGTH_LONG)
                             .show()
@@ -46,10 +66,10 @@ class TopChartFragment : Fragment() {
                 }
 
                 is Resource.Loading -> {
-
+                    progressBar.isVisible = true
+                    recyclerView.isVisible = false
                 }
             }
-
         }
 
         return view
